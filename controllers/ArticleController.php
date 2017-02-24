4b77a16753944271b5ad2e705804fdd1cf0a2992
UR1991 <ultimaratio1991@gmail.com>
2017-02-24 21:53:24 +0300

    Add controllers and models for articles and comments

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,3 +1,36 @@
 <?php
 //Controller for working with articles
+class ArticleController extends Controller
+{
+  public function actionIndex($value='')
+  {
+    # code...
+  }
+
+  public function actionCreate($value='')
+  {
+    # code...
+  }
+
+  public function actionRead($value='')
+  {
+    # code...
+  }
+
+  public function actionUpdate($value='')
+  {
+    # code...
+  }
+
+  public function actionDelete($value='')
+  {
+    # code...
+  }
+
+  public function actionSubscribe($value='')
+  {
+    # code...
+  }
+}
+
  ?>
---
+++ controllers/CommentController.php
@@ -0,0 +1,21 @@
+<?php
+//Controller for working with comments
+
+class CommentController extends Controller
+{
+  public function actionIndex($value='')
+  {
+    # code...
+  }
+
+  public function actionCreate($value='')
+  {
+    # code...
+  }
+
+  public function actionDelete($value='')
+  {
+    # code...
+  }
+}
+ ?>
